8d073c8b31e0a516a465223311132ffb024c04e7
Baptiste <baptiste.colin@outlook.com>
2017-11-06 00:08:54 +0100

    some comments

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,14 @@
 fn main() {
+    // about parsing (here what's important is that the type of num had to be specified beforehand)
     let num: u32 = "42".parse().expect("Not a number !");
     println!("Parsed as {}", num);
 
+    // about scalar types
     let x = 2.0; // f64
     let y: f32 = 3.0; // f32
-    
-
     let a: u32 = 2;
     let b: u32 = 3;
+
     let z = x/y;
     println!("Le résultat est {}", z);
     let z = a/b;
@@ -21,6 +22,8 @@ fn main() {
     //let z = a/y;
     //println!("Le résultat est {}", z);
 
+
+    // about operations
     let sum = 5 + 10;
     let difference = 95.5 - 4.3;
     let product = 4 * 30;
@@ -33,6 +36,8 @@ fn main() {
     println!("quotient : {}", quotient);
     println!("remainder : {}", remainder);
 
+    // about booleans
+    //
     let t = true;
     let f: bool = false;
     let or = t || f;
