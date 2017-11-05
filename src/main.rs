f2dc78b8c4dda8ff8beed83b9d425077b273c767
Baptiste <baptiste.colin@outlook.com>
2017-11-06 00:01:04 +0100

    practicing scalar type compatibility

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,23 @@
 fn main() {
     let num: u32 = "42".parse().expect("Not a number !");
-    println!("{}", num);
+    println!("Parsed as {}", num);
+
+    let x = 2.0; // f64
+    let y: f32 = 3.0; // f32
+    
+
+    let a: u32 = 2;
+    let b: u32 = 3;
+    let z = x/y;
+    println!("Le résultat est {}", z);
+    let z = a/b;
+    println!("Le résultat est {}", z);
+    let z = b/a;
+    println!("Le résultat est {}", z);
+
+    // Those below wouldn't work because of incompatibility between floats and ints
+    //let z = x/b;
+    //println!("Le résultat est {}", z);
+    //let z = a/y;
+    //println!("Le résultat est {}", z);
 }
