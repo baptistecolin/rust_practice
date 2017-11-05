3dc46dfd26737a83f0b0c8dc374f90b3ac2166cc
Baptiste <baptiste.colin@outlook.com>
2017-11-06 00:04:23 +0100

    practicing operations

--- src/main.rs
+++ src/main.rs
@@ -20,4 +20,16 @@ fn main() {
     //println!("Le résultat est {}", z);
     //let z = a/y;
     //println!("Le résultat est {}", z);
+
+    let sum = 5 + 10;
+    let difference = 95.5 - 4.3;
+    let product = 4 * 30;
+    let quotient = 56.7 / 32.2;
+    let remainder = 43 % 5;
+
+    println!("sum : {}", sum);
+    println!("difference : {}", difference);
+    println!("product : {}", product);
+    println!("quotient : {}", quotient);
+    println!("remainder : {}", remainder);
 }
